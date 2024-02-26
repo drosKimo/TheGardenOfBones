0132000ab496e2c7b50e6cb529560a817af02f1a
drosKimo <ilikeuflower@yandex.ru>
2024-02-27 00:32:27 +0300

    NavMesh & Tile update

--- Assets/Scripts/Creatures/CS_SpiritController.cs
+++ Assets/Scripts/Creatures/CS_SpiritController.cs
@@ -1,30 +1,64 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class CS_SpiritController : MonoBehaviour
 {
     [HideInInspector] public Animator spiritAnim;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float speed;
+
     CS_SpiritHelp spiritHelp;
+    NavMeshAgent meshAgent;
+    SpriteRenderer spiritRender;
+
+    Vector3 player_pos;
+    bool guided = false;
+    float distance = 1.6f;
 
     private void Awake()
     {
         spiritAnim = GetComponent<Animator>();
         spiritHelp = GetComponentInChildren<CS_SpiritHelp>();
+        meshAgent = GetComponent<NavMeshAgent>();
+        spiritRender = gameObject.GetComponent<SpriteRenderer>();
+
+        meshAgent.updateRotation = false;
+        meshAgent.updateUpAxis = false;
     }
     private void Update()
     {
         switch (spiritAnim.GetInteger("SetSpirit"))
         {
             case 1:
+                guided = true;
                 spiritAnim.SetInteger("SetSpirit", 0);
                 spiritHelp.anim.SetTrigger("Left");
                 StartCoroutine(HelpDestroyer());
                 break;
             case 2:
+                guided = false;
                 spiritAnim.SetInteger("SetSpirit", 0);
                 spiritAnim.SetTrigger("Planted");
+                meshAgent.isStopped = true; // чтобы остановить агента, если он далеко. ВРЕМЕННО
                 StartCoroutine (TriggerDestroyer());
                 break;
+        }
+
+        if (guided == true)
+        {
+            player_pos = playerTransform.position;
+
+            meshAgent.destination = player_pos;
+            meshAgent.stoppingDistance = distance;
+
+            if ((gameObject.transform.position - player_pos).x > 0) // сравнение позиций относительно друг друга
+                spiritRender.flipX = true; // моделька разворачивается
+            else spiritRender.flipX = false;
+
+            spiritAnim.SetBool(name: "GoRight", value: meshAgent.remainingDistance > distance); // двигается только когда расстояние больше дистанции остановки
         }
     }
 
